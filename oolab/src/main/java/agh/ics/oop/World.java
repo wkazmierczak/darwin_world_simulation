448a3a57e1dd3e05cca8a2d3d5e46b6c10d6ac57
wkazmierczak <119811949+wkazmierczak@users.noreply.github.com>
2023-10-12 10:09:00 +0200

    improve run() function

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -9,19 +9,28 @@ public class World {
     }
 
     public static void run(String[] arguments){
-        System.out.println("Zwierzak idzie do przodu");
-        int i = 0;
-        int l = arguments.length;
+//        System.out.println("Zwierzak idzie do przodu");
+//        int i = 0;
+//        int l = arguments.length;
+        System.out.println("Start");
         for (String argument: arguments){
-            System.out.print(argument);
-            i++;
-            if(i < l){
-                System.out.print(", ");
-            }
-            else{
-                System.out.print("\n");
-            }
+//            System.out.print(argument);
+//            i++;
+//            if(i < l){
+//                System.out.print(", ");
+//            }
+//            else{
+//                System.out.print("\n");
+//            }
+            switch (argument) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tyłu");
+                case "r" -> System.out.println("Zwierzak skręca w prawo");
+                case "l" -> System.out.println("Zwierzak skręca w lewo");
+            };
+
         }
+        System.out.println("Stop");
 
     }
 }
