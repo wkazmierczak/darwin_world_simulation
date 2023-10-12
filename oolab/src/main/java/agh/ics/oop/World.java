bb51f6813ae4259371d3054d1ff8b63c2ca5bd01
wkazmierczak <119811949+wkazmierczak@users.noreply.github.com>
2023-10-12 09:58:25 +0200

    add basic run() function

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,7 +3,25 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args){
         System.out.println("system wystartował");
+        run(args);
         System.out.println("system zakończył działanie");
+
+    }
+
+    public static void run(String[] arguments){
+        System.out.println("Zwierzak idzie do przodu");
+        int i = 0;
+        int l = arguments.length;
+        for (String argument: arguments){
+            System.out.print(argument);
+            i++;
+            if(i < l){
+                System.out.print(", ");
+            }
+            else{
+                System.out.print("\n");
+            }
+        }
 
     }
 }
