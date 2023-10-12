72542871d8dbda9fe2590babfc956d2fff848e41
wkazmierczak <119811949+wkazmierczak@users.noreply.github.com>
2023-10-12 09:40:01 +0200

    initialize project and create first class

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args){
+        System.out.println("system wystartował");
+        System.out.println("system zakończył działanie");
+
+    }
+}
