313dbdc2531a8baa220334245dc6b336ad03ad2a
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-01-08 12:32:41 +0100

    planet map tests

--- darwin_world/src/test/java/agh/ics/oop/model/maps/AbstractPlanetMapTest.java
+++ darwin_world/src/test/java/agh/ics/oop/model/maps/AbstractPlanetMapTest.java
@@ -2,57 +2,21 @@ package agh.ics.oop.model.maps;
 
 import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.genotype.BasicGenotype;
+import agh.ics.oop.model.genotype.GenotypeType;
+import agh.ics.oop.model.setupData.AnimalSetupData;
+import agh.ics.oop.model.util.MyRange;
 import agh.ics.oop.model.worldElements.Animal;
 import agh.ics.oop.model.worldElements.PositionDetails;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractPlanetMapTest {
-
-    @Test
-    void move() {
-    }
-
-    @Test
-    void addListener() {
-    }
-
-    @Test
-    void removeListener() {
-    }
-
-    @Test
-    void mapChanged() {
-    }
-
-    @Test
-    void place() {
-    }
-
-    @Test
-    void testPlace() {
-    }
-
-    @Test
-    void animalsAt() {
-    }
-
-    @Test
-    void nextDay() {
-    }
-
-    @Test
-    void growPlants() {
-    }
-
-    @Test
-    void testGrowPlants() {
-    }
-
-    @Test
-    void plantAt() {
-    }
 
     @Test
     void moveIntoDirection() {
@@ -101,61 +65,40 @@ class AbstractPlanetMapTest {
 
     @Test
     void removeDead() {
-    }
+        PlanetMap map = new EquatorMap(5, 5);
+        AnimalSetupData setupData = new AnimalSetupData(1, -10, 1, 1, new MyRange(10, 20), 1, GenotypeType.BASIC_GENOTYPE);
+        Animal deadAnimal = new Animal(new Vector2d(2, 2), setupData);
+        Animal deadAnimal1 = new Animal(new Vector2d(1, 2), setupData);
+        Animal deadAnimal2 = new Animal(new Vector2d(2, 3), setupData);
+        List<Animal> animals = List.of(deadAnimal, deadAnimal1, deadAnimal2);
+        for (Animal animal : animals) {
+            map.place(animal);
+        }
 
-    @Test
-    void removeAnimal() {
+        List<Animal> removed = map.removeDead(animals, 1);
+
+        assertEquals(3, removed.size());
     }
 
     @Test
     void handleWhoEats() {
+        PlanetMap map = new EquatorMap(5, 5);
+        AnimalSetupData setupData = new AnimalSetupData(1, 0, 1, 1, new MyRange(10, 20), 1, GenotypeType.BASIC_GENOTYPE);
+        Animal parent1 = new Animal(new Vector2d(2, 2), setupData);
+        Animal parent2 = new Animal(new Vector2d(2, 2), setupData);
+        Animal contender1 = new Animal(10, new BasicGenotype(1), parent1, parent2);
+        Animal contender2 = new Animal(15, new BasicGenotype(1), parent1, parent2);
+        Animal contender3 = new Animal(10, new BasicGenotype(1), parent1, parent2);
+        Animal contender4 = new Animal(10, new BasicGenotype(1), parent1, parent2);
+        contender1.getStats().incrementAge();
+        contender4.getStats().addChild(contender1);
+
+        map.place(contender1);
+        map.place(contender2);
+        map.place(contender3);
+        map.place(contender4);
+        Collection<Animal> animals = map.animalsAt(new Vector2d(2, 2));
+        assertIterableEquals(List.of(contender2, contender1, contender4, contender3), animals);
     }
 
-    @Test
-    void letAnimalsEat() {
-    }
-
-    @Test
-    void handleWhoReproduces() {
-    }
-
-    @Test
-    void letAnimalsReproduce() {
-    }
-
-    @Test
-    void removePlant() {
-    }
-
-    @Test
-    void getPlantsCount() {
-    }
-
-    @Test
-    void getSetupData() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void getCurrentBounds() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void getFreePositionsCount() {
-    }
-
-    @Test
-    void getStartingPlantsCount() {
-    }
-
-    @Test
-    void getPlantsPerDay() {
-    }
 }
