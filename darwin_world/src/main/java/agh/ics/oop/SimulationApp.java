c591db0a60179036b0202779653dccefd72519ba
wkazmierczak <119811949+wkazmierczak@users.noreply.github.com>
2024-01-15 13:36:04 +0100

    GUI base setupData window

--- darwin_world/src/main/java/agh/ics/oop/SimulationApp.java
+++ darwin_world/src/main/java/agh/ics/oop/SimulationApp.java
@@ -1,42 +1,71 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.*;
+import agh.ics.oop.model.setupData.SimulationSetupData;
+import agh.ics.oop.presenter.EntryScreenPresenter;
 import agh.ics.oop.presenter.SimulationPresenter;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.List;
 
 public class SimulationApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
-        BorderPane viewRoot = loader.load();
-        configureStage(primaryStage, viewRoot);
-        SimulationPresenter presenter = loader.getController();
-
-//        String[] input1 = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "l", "f", "r", "f", "f", "b", "b"};
-//        GrassField map2 = new GrassField(10);
-//        List<MoveDirection> directions2 = OptionsParser.parse(input1);
-//        List<Vector2d> positions2 = List.of(new Vector2d(2,2), new Vector2d(3,2), new Vector2d(-10, 2));
-
-
-//        presenter.setWorldMap(map2);
-//        map2.addListener(presenter);
-
-
-//        Simulation simulation2 = new Simulation(directions2, positions2, map2);
-
-
-//        simulation2.run();
+        loader.setLocation(getClass().getClassLoader().getResource("entryScreen.fxml"));
+        GridPane viewRoot = loader.load();
+        configureStageGridPane(primaryStage, viewRoot);
+        EntryScreenPresenter presenter = loader.getController();
         primaryStage.show();
 
+        Button submitButton = presenter.getSubmitButton();
+        submitButton.setOnAction(e -> {
+            presenter.getInput();
+            SimulationSetupData setupData = presenter.getSetupData();
+//            primaryStage.close();
+
+            openSimWindow(setupData);
+        });
+        System.out.println(4);
+
+    }
+    private void openSimWindow(SimulationSetupData setupData) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+            Stage scndStage = new Stage();
+            BorderPane viewRoot = loader.load();
+            configureStage(scndStage, viewRoot);
+            SimulationPresenter simulationPresenter = loader.getController();
+            System.out.println(setupData);
+            simulationPresenter.setSetupData(setupData);
+
+            scndStage.show();
+
+        } catch (Exception e) {
+            System.out.println("Can't load new window: " + e.getMessage());
+        }
+    }
+
+
+    private void configureStageGridPane(Stage primaryStage, GridPane viewRoot) {
+        var scene = new Scene(viewRoot);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Simulation app");
+        viewRoot.setHgap(10);
+        viewRoot.setVgap(10);
+        viewRoot.setPadding(new Insets(10, 10, 10, 10));
     }
 
     private void configureStage(Stage primaryStage, BorderPane viewRoot) {
--- darwin_world/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ darwin_world/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,10 +1,19 @@
 package agh.ics.oop.presenter;
 
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.Simulation.Simulation;
+import agh.ics.oop.Simulation.SimulationEngine;
 import agh.ics.oop.model.*;
 
+import agh.ics.oop.model.genotype.GenotypeType;
+import agh.ics.oop.model.listeners.AnimalChangeListener;
 import agh.ics.oop.model.listeners.MapChangeListener;
+import agh.ics.oop.model.listeners.SimulationChangeListener;
+import agh.ics.oop.model.maps.MapType;
 import agh.ics.oop.model.maps.PlanetMap;
 
+import agh.ics.oop.model.setupData.SimulationSetupData;
+import agh.ics.oop.model.util.MyRange;
 import agh.ics.oop.model.worldElements.Animal;
 import agh.ics.oop.model.worldElements.WorldElement;
 import javafx.application.Platform;
@@ -18,10 +27,29 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
-public class SimulationPresenter implements MapChangeListener {
+import java.util.List;
+
+public class SimulationPresenter implements MapChangeListener, AnimalChangeListener, SimulationChangeListener {
     private static final double CELL_WIDTH = 30 ;
     private static final double CELL_HEIGHT = 30;
     private static PlanetMap map;
+    public Label genotype;
+    public Label dayOfSim;
+    public Label numOfAnimals;
+    public Label numOfPlants;
+    public Label numOFreeSpots;
+    public Label mostCommonGenotype;
+    public Label avgEnergyLevel;
+    public Label avgLifespan;
+    public Label avgChildrenNum;
+    public Label activeGenotype;
+    public Label numOfEatenPlants;
+    public Label numOfChildren;
+    public Label numOfDescendants;
+    public Label energyLevel;
+    public Label age;
+    public Label dayOfDeath;
+    private SimulationSetupData setupData;
 
     @FXML
     public Button startButton;
@@ -39,17 +67,77 @@ public class SimulationPresenter implements MapChangeListener {
         this.map = map;
     }
 
-    public void drawMap(){
-
-        clearGrid();
-
-
+    public void simulationSettings(){
         Vector2d lowerLeft = map.getCurrentBounds().bottomLeft();
         Vector2d upperRight = map.getCurrentBounds().upperRight();
 
         int rows = upperRight.getY() - lowerLeft.getY() + 1;
         int columns = upperRight.getX() - lowerLeft.getX() + 1;
 
+        for (int i = 0; i < columns+1; i++) {
+            mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
+        }
+
+        for (int i = 0; i < rows+1; i++) {
+            mapGrid.getRowConstraints().add(new RowConstraints(CELL_HEIGHT));
+        }
+
+        Label cellLabel1 = new Label();
+        cellLabel1.setMinSize(10, 10);
+        cellLabel1.setText("y\\x");
+        mapGrid.add(cellLabel1, 0, 0);
+        GridPane.setHalignment(cellLabel1, HPos.CENTER);
+
+        for (int i = 1; i < columns+1; i++) {
+            Label cellLabel = new Label();
+            cellLabel.setMinSize(10, 10);
+            cellLabel.setText("" + (lowerLeft.getX()+i-1));
+            mapGrid.add(cellLabel, i, 0);
+            GridPane.setHalignment(cellLabel, HPos.CENTER);
+
+        }
+
+        for (int i = 1; i < rows+1; i++) {
+            Label cellLabel = new Label();
+            cellLabel.setMinSize(10, 10);
+            cellLabel.setText("" + (upperRight.getY()-i+1));
+            mapGrid.add(cellLabel, 0, i);
+            GridPane.setHalignment(cellLabel, HPos.CENTER);
+        }
+
+        int col = 1;
+        int row;
+
+        for (int x = lowerLeft.getX();x<=upperRight.getX();x++) {
+            row = 1;
+            for (int y = upperRight.getY();y>=lowerLeft.getY();y--) {
+
+                Vector2d currentPosition = new Vector2d(x, y);
+                WorldElement elem = (WorldElement) map.plantAt(currentPosition);
+                if (elem != null){
+                    Label cellLabel = new Label();
+                    cellLabel.setMinSize(10, 10);
+                    cellLabel.setText(map.plantAt(currentPosition).toString());
+                    mapGrid.add(cellLabel, col,row);
+                    GridPane.setHalignment(cellLabel, HPos.CENTER);
+
+
+                }
+                row++;
+            }
+            col++;
+        }
+    }
+
+    public void drawMap(){
+
+        clearGrid();
+
+        Vector2d lowerLeft = map.getCurrentBounds().bottomLeft();
+        Vector2d upperRight = map.getCurrentBounds().upperRight();
+
+        int rows = upperRight.getY() - lowerLeft.getY() + 1;
+        int columns = upperRight.getX() - lowerLeft.getX() + 1;
 
         for (int i = 0; i < columns+1; i++) {
             mapGrid.getColumnConstraints().add(new ColumnConstraints(CELL_WIDTH));
@@ -113,7 +201,6 @@ public class SimulationPresenter implements MapChangeListener {
     @Override
     public void mapChanged(PlanetMap worldMap, String message) {// zmiana na animal
 
-
         Platform.runLater(() -> {
             infoLabel.setText(message);
             drawMap();
@@ -121,28 +208,66 @@ public class SimulationPresenter implements MapChangeListener {
     }
 
     public void onSimulationStartClicked(ActionEvent actionEvent) {
-//
-//
-//        String[] input1 = textField.getText().split(" ");
-//
-//        List<MoveDirection> directions2 = OptionsParser.parse(input1);
-//        List<Vector2d> positions2 = List.of(new Vector2d(2, 2), new Vector2d(-3,2));
-//
-//
-//        Simulation simulation2 = new Simulation(directions2, positions2, map);
-//        List<Simulation> sims = List.of(simulation2);
-//        SimulationEngine engine = new SimulationEngine(sims);
-////        engine.runSync();
-//        engine.runAsync();
+
+
+        Simulation simulation2 = new Simulation(setupData);
+        simulation2.addSimulationChangeListener(this);
+        List<Simulation> sims = List.of(simulation2);
+        SimulationEngine engine = new SimulationEngine(sims);
+//        engine.runSync();
+        engine.runAsyncInThreadPool();
 //        engine.awaitSimulationEnd();
-//
-//
-////        simulation2.run();
+
+
+//        simulation2.run();
     }
+
+
 
     private void clearGrid() {
         mapGrid.getChildren().retainAll(mapGrid.getChildren().get(0)); // hack to retain visible grid lines
         mapGrid.getColumnConstraints().clear();
         mapGrid.getRowConstraints().clear();
     }
+
+    public SimulationSetupData getSetupData() {
+        return setupData;
+    }
+
+    public void setSetupData(SimulationSetupData setupData) {
+        this.setupData = setupData;
+    }
+
+    //TODO dont know how to connect animal with animal listener
+    @Override
+    public synchronized void animalInfoChanged(Animal animal) {
+        Platform.runLater(()->{
+            genotype.setText(String.valueOf(animal.getGenotype().getGenotypeList()));
+            activeGenotype.setText(String.valueOf(animal.getGenotype().next()));
+            energyLevel.setText(String.valueOf(animal.getEnergyLevel()));
+            numOfEatenPlants.setText(String.valueOf(animal.getStats().getPlantsEaten()));
+            numOfChildren.setText(String.valueOf(animal.getStats().getChildrenCount()));
+            numOfDescendants.setText(String.valueOf(animal.getStats().getDescendantsCount()));
+            age.setText(String.valueOf(animal.getStats().getAge()));
+            dayOfDeath.setText(String.valueOf(animal.getStats().getDayOfDeath()));
+        });
+    }
+
+    @Override
+    public synchronized void simulationChanged(Simulation simulation) {
+        Platform.runLater(()-> {
+            dayOfSim.setText(String.valueOf(simulation.getDayOfSimulation()));
+            numOfAnimals.setText(String.valueOf(simulation.getAnimals().size()));
+            numOfPlants.setText(String.valueOf(simulation.getWorldMap().getPlantsCount()));
+            numOFreeSpots.setText(String.valueOf(simulation.getWorldMap().getFreePositionsCount()));
+            mostCommonGenotype.setText(String.valueOf(simulation.getStatsController().getMostPopularGenotype()));
+            avgEnergyLevel.setText(String.valueOf(simulation.getStatsController().getAvgEnergyLevel()));
+            avgLifespan.setText(String.valueOf(simulation.getStatsController().getAvgLifespanForDeadAnimals()));
+            avgChildrenNum.setText(String.valueOf(simulation.getStatsController().getAvgNumOfChildrenForAliveAnimals(simulation.getAnimals())));
+        });
+    }
+
+//    public void setAnimalToTrack(Animal animalToTrack){
+//        animalToTrack.addAnimalTracker(this);
+//    }
 }
