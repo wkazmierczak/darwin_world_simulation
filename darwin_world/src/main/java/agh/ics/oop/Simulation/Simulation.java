da99478761129a299071be5caed145e55a10b282
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-01-15 22:06:43 +0100

    most popular genotype highlight

--- darwin_world/src/main/java/agh/ics/oop/Simulation/Simulation.java
+++ darwin_world/src/main/java/agh/ics/oop/Simulation/Simulation.java
@@ -53,11 +53,17 @@ public class Simulation implements Runnable {
     public void run() {
         while (statsController.getDayOfSimulation() < simulationSetupData.maxDays()) {
             nextDay();
+            System.out.println("Day " + getDayOfSimulation() + " of simulation " + id);
             removeDead();
+            System.out.println("removed dead");
             moveAnimals();
+            System.out.println("moved animals");
             worldMap.letAnimalsEat();
+            System.out.println("animals ate");
             letReproduce();
+            System.out.println("animals reproduced");
             worldMap.growPlants();
+            System.out.println("plants grew");
 
             notifySimulationChanged(this);
 
@@ -75,11 +81,12 @@ public class Simulation implements Runnable {
                         throw new RuntimeException(e);
                     }
                 }
-            }try { //tymczasowe
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             }
+//            try { //tymczasowe
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }
 
     }
@@ -99,8 +106,7 @@ public class Simulation implements Runnable {
     }
 
     private void letReproduce() {
-        List<Animal> newborns = worldMap.letAnimalsReproduce();
-        animals.addAll(newborns);
+        worldMap.letAnimalsReproduce().forEach(animals::add);
     }
 
     private void moveAnimals() {
